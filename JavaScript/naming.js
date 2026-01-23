9b1237c2b0d6209e0baa17a29b2a405fb53ba1a8
Hafizur-Rahman-SD <hrrony859@gmail.com>
2026-01-23 16:15:31 +0600

    naming system in varibal

---
+++ JavaScript/naming.js
@@ -0,0 +1,29 @@
+
+//all are error in this file 
+
+
+
+//no keyword use in varibale naming
+var var afizur Rahman";
+console.log(name);  //String type
+
+//no space or gap in js variable naming
+ var Is happy =true;
+ console.log(Is happy);  //boolean type
+
+//no quatation use in variable naming
+ var 'name' ="Hafizur Rahman";
+ console.log('name');  //String type
+
+
+ //cannot start with number in variable naming
+ var 9age =24;
+ console.log(9age);   //Number type 
+
+
+ //case sensitive variable naming
+    var Name ="Hafizur Rahman";
+    var name ="Rahman Hafizur";
+    console.log(Name);  //String type
+    console.log(name);  //String type
+
