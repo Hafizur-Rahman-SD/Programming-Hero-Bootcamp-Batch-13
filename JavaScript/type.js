cf9eaeea8a72b44a15a7f2ffccacfb797882410c
Hafizur-Rahman-SD <hrrony859@gmail.com>
2026-01-23 16:07:13 +0600

    this is 2nd commit in JS

---
+++ JavaScript/type.js
@@ -0,0 +1,20 @@
+var name ="Hafizur Rahman";
+console.log(name);  //String type
+
+var age = 24;
+console.log(age);   //Number type
+
+//boolean type
+var isSingle = true;  //if we use "True" then it will be consider as string type
+console.log(isSingle);  //boolean type
+
+//Object type
+var student = {id:101, phone:01712345678, name:"Hafizur Rahman"};
+console.log(student);  //object type
+
+//type of 
+console.log(typeof name);  //String type
+console.log(typeof age);   //Number type
+console.log(typeof isSingle);
+console.log(typeof student);  //boolean type
+console.log(typeof student);  //object type
