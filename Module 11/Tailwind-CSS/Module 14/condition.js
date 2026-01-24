ac2960adf1f5e807ca655274a46239f2a17d3ca3
Hafizur-Rahman-SD <hrrony859@gmail.com>
2026-01-24 19:25:06 +0600

    new module start to see and practice

---
+++ Module 11/Tailwind-CSS/Module 14/condition.js
@@ -0,0 +1,22 @@
+
+// if(condition){
+//     // code to be executed if condition is true
+// }
+
+
+
+if (13<10) {
+    console.log("13 is less than 10");
+}else {
+    console.log("13 is not less than 10");
+}
+
+
+
+
+var age = 18;
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
