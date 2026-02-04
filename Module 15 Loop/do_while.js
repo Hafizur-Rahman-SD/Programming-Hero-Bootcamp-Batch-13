3ab83dbdd38ec4ba3a62a35ebad6c78c428aa037
Hafizur-Rahman-SD <hrrony859@gmail.com>
2026-02-04 13:35:02 +0600

    module 15 done loop

---
+++ Module 15 Loop/do_while.js
@@ -0,0 +1,19 @@
+
+
+//Do while loop example its almost no use 
+let count = 1;
+do {
+    console.log("Count is:", count);
+    count++;
+} while (count <= 5);
+
+
+let i =10;
+do {
+    console.log("Value of i is:", i);
+    i++;    
+} while (i <= 5); //this will run once even condition is false
+//output will be Value of i is: 10
+
+
+//its must 1 time run even condition is false
