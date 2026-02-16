150d241ce9b3169d5ab270e914b9cc287c17cf1b
Hafizur-Rahman-SD <hrrony859@gmail.com>
2026-02-16 21:40:58 +0600

    add

---
+++ Module 17 Function/matched.js
@@ -0,0 +1,10 @@
+const numbers=[45,20,23,98,100,7,13,11];
+
+// for (let i=0;i<numbers.length; i++){
+//     const number =numbers[i];
+//     console.log(number);
+// }
+
+for(const number of numbers){
+
+}
