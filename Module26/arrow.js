8de481860a26ba8187aed793772cebf682094791
Hafizur-Rahman-SD <hrrony859@gmail.com>
2026-02-26 21:46:07 +0600

    Create arrow.js

---
+++ Module26/arrow.js
@@ -0,0 +1,15 @@
+
+//for add with arrow function 
+const add= (num1,num2) => num1+num2;
+console.log (add(10,20));
+
+const multiply = (num3, num4) => num3 * num4;
+console.log(multiply(2,3));
+
+
+const doMath = (x,y) =>{
+    const sum = (x+y);
+    const diff = x-y;
+
+}
+console.log(doMath(10,20));
