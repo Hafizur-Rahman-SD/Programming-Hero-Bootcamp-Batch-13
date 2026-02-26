24ee6d6e0e674189ed5977417564a1091d89b3df
Hafizur-Rahman-SD <hrrony859@gmail.com>
2026-02-26 21:23:35 +0600

    Update FunctionParamater.js

--- Module26/FunctionParamater.js
+++ Module26/FunctionParamater.js
@@ -1,5 +1,12 @@
-function add (num1,num2){
-const total=num1+num2;
-console.log(num1,num2,total);
+// function add (num1,num2){
+// const total=num1+num2;
+// console.log(num1,num2,total);
+
+// }
+
+function FullName( first,last=' '){
+    const name=first+' ' +last;
+    console.log(name);
 
 }
+FullName('Hafizur', 'Rahman')
